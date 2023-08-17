ae535e089abe2e9d7e3ce616558630fc9ed770a2
AnsonYe <anson.ye@gamil.com>
2023-08-17 15:43:18 +0900

    vpc

---
+++ 0-provider.tf
@@ -0,0 +1,23 @@
+# Provider with some variables such as EKS cluster name and a region.
+variable "cluster_name" {
+  default = "demo"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_version = "~> 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.6"
+    }
+  }
+}
